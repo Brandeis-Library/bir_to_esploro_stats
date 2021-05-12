d8918e3b3050f785db4cf36f9c35fcf51e001459
Chris Underwood <cunderwoodmn@gmail.com>
2021-05-11 19:22:00 -0700

    moved in data from legacy bir stats. created plan version 1 of the app to process the stats into the proper format.

---
+++ legacy_dspace_stats/index.js
@@ -0,0 +1,13 @@
+// convert http://bir.brandeis.edu/handle/10192/3044
+// to      oai:bir.brandeis.edu:10192/3044
+
+// Going to use the Digital Commons import usage data by origination system id.
+// One column of the org system id. other columns for views/month
+
+// is oai the asset originating system id in Esploro?
+
+// grab table with <th> Number of views text
+// in each tr
+//    td-0 grab text of td
+//    td-1 grab text of views/downloads
+// convert url from td-0 to Esploro friendly format
