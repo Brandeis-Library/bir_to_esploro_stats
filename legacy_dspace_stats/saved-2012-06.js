068ab08be184aaa6be7d22247423aca7af04e865
Chris Underwood <cunderwoodmn@gmail.com>
2021-05-24 18:56:18 -0700

    files created to have the individual stats for the 1st 3 reports for testing of the loop.

---
+++ legacy_dspace_stats/saved-2012-06.js
@@ -0,0 +1,417 @@
+exports.dataObjs = [
+  {
+    handle: 'https://hdl.handle.net/10192/4530',
+    count: ' 169 ',
+    name: '/10192/4530_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/77',
+    count: ' 131 ',
+    name: '/10192/77_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/61',
+    count: ' 73 ',
+    name: '/10192/61_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/3144',
+    count: ' 62 ',
+    name: '/10192/3144_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/44',
+    count: ' 55 ',
+    name: '/10192/44_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22924',
+    count: ' 53 ',
+    name: '/10192/22924_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/1854',
+    count: ' 47 ',
+    name: '/10192/1854_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/80',
+    count: ' 40 ',
+    name: '/10192/80_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/83',
+    count: ' 40 ',
+    name: '/10192/83_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/81',
+    count: ' 37 ',
+    name: '/10192/81_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22948',
+    count: ' 37 ',
+    name: '/10192/22948_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/60',
+    count: ' 37 ',
+    name: '/10192/60_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23678',
+    count: ' 36 ',
+    name: '/10192/23678_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22986',
+    count: ' 36 ',
+    name: '/10192/22986_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22926',
+    count: ' 34 ',
+    name: '/10192/22926_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/82',
+    count: ' 33 ',
+    name: '/10192/82_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/24365',
+    count: ' 33 ',
+    name: '/10192/24365_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/24237',
+    count: ' 32 ',
+    name: '/10192/24237_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/4962',
+    count: ' 31 ',
+    name: '/10192/4962_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22991',
+    count: ' 31 ',
+    name: '/10192/22991_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/71',
+    count: ' 31 ',
+    name: '/10192/71_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/1846',
+    count: ' 30 ',
+    name: '/10192/1846_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/59',
+    count: ' 30 ',
+    name: '/10192/59_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/36',
+    count: ' 29 ',
+    name: '/10192/36_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/24386',
+    count: ' 29 ',
+    name: '/10192/24386_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23212',
+    count: ' 29 ',
+    name: '/10192/23212_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/62',
+    count: ' 28 ',
+    name: '/10192/62_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23017',
+    count: ' 28 ',
+    name: '/10192/23017_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/63',
+    count: ' 27 ',
+    name: '/10192/63_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23818',
+    count: ' 27 ',
+    name: '/10192/23818_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23862',
+    count: ' 27 ',
+    name: '/10192/23862_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22963',
+    count: ' 27 ',
+    name: '/10192/22963_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/3033',
+    count: ' 27 ',
+    name: '/10192/3033_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/30',
+    count: ' 26 ',
+    name: '/10192/30_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/3784',
+    count: ' 26 ',
+    name: '/10192/3784_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/49',
+    count: ' 26 ',
+    name: '/10192/49_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/5029',
+    count: ' 25 ',
+    name: '/10192/5029_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23882',
+    count: ' 25 ',
+    name: '/10192/23882_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/79',
+    count: ' 25 ',
+    name: '/10192/79_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22995',
+    count: ' 25 ',
+    name: '/10192/22995_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23819',
+    count: ' 25 ',
+    name: '/10192/23819_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23170',
+    count: ' 24 ',
+    name: '/10192/23170_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/24079',
+    count: ' 24 ',
+    name: '/10192/24079_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22999',
+    count: ' 24 ',
+    name: '/10192/22999_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/4526',
+    count: ' 24 ',
+    name: '/10192/4526_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/4322',
+    count: ' 24 ',
+    name: '/10192/4322_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22927',
+    count: ' 24 ',
+    name: '/10192/22927_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23877',
+    count: ' 24 ',
+    name: '/10192/23877_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23380',
+    count: ' 24 ',
+    name: '/10192/23380_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22974',
+    count: ' 24 ',
+    name: '/10192/22974_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/1723',
+    count: ' 23 ',
+    name: '/10192/1723_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/16',
+    count: ' 23 ',
+    name: '/10192/16_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/52',
+    count: ' 23 ',
+    name: '/10192/52_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/24178',
+    count: ' 22 ',
+    name: '/10192/24178_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23800',
+    count: ' 22 ',
+    name: '/10192/23800_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/24297',
+    count: ' 22 ',
+    name: '/10192/24297_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22985',
+    count: ' 22 ',
+    name: '/10192/22985_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/22928',
+    count: ' 22 ',
+    name: '/10192/22928_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/3680',
+    count: ' 21 ',
+    name: '/10192/3680_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/32',
+    count: ' 21 ',
+    name: '/10192/32_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23007',
+    count: ' 21 ',
+    name: '/10192/23007_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23867',
+    count: ' 21 ',
+    name: '/10192/23867_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/58',
+    count: ' 21 ',
+    name: '/10192/58_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/51',
+    count: ' 21 ',
+    name: '/10192/51_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/18',
+    count: ' 21 ',
+    name: '/10192/18_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23070',
+    count: ' 20 ',
+    name: '/10192/23070_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/3724',
+    count: ' 20 ',
+    name: '/10192/3724_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/3722',
+    count: ' 20 ',
+    name: '/10192/3722_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/4435',
+    count: ' 20 ',
+    name: '/10192/4435_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/33',
+    count: ' 20 ',
+    name: '/10192/33_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/64',
+    count: ' 20 ',
+    name: '/10192/64_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23762',
+    count: ' 20 ',
+    name: '/10192/23762_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/24',
+    count: ' 20 ',
+    name: '/10192/24_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23918',
+    count: ' 20 ',
+    name: '/10192/23918_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/24397',
+    count: ' 20 ',
+    name: '/10192/24397_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23866',
+    count: ' 20 ',
+    name: '/10192/23866_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23797',
+    count: ' 20 ',
+    name: '/10192/23797_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23002',
+    count: ' 20 ',
+    name: '/10192/23002_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/2151',
+    count: ' 20 ',
+    name: '/10192/2151_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23647',
+    count: ' 20 ',
+    name: '/10192/23647_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/23440',
+    count: ' 20 ',
+    name: '/10192/23440_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/1867',
+    count: ' 20 ',
+    name: '/10192/1867_2012_06',
+  },
+  {
+    handle: 'https://hdl.handle.net/10192/24317',
+    count: ' 20 ',
+    name: '/10192/24317_2012_06',
+  },
+];
