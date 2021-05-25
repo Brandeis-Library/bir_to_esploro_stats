0e7a728a15e6d17a1d863c090d9e3737e42426bf
Chris Underwood <cunderwoodmn@gmail.com>
2021-05-25 16:43:34 -0700

    continued work on the looping over the arrary of handles to find statsObjs with little progress so far.

--- legacy_dspace_stats/index3.js
+++ legacy_dspace_stats/index3.js
@@ -1,21 +1,50 @@
+const fs = require('fs');
+
 (async function () {
   try {
     const { statsObjs } = require('./processedObjs.js');
     const { handles } = require('./handleList.js');
 
+    const year_month = '_2012_04';
+
     fs.truncateSync('./individualStats.csv');
     //console.log(statsObjs);
     let num = 0;
-    for (handle in statsObjs) {
-      console.log(
-        num,
-        ' handle: ',
-        statsObjs[handle].handle,
-        'count: ',
-        statsObjs[handle].count
-      );
-      num++;
-    }
+    // for (handle in statsObjs) {
+    //   console.log(
+    //     num,
+    //     ' handle: ',
+    //     statsObjs[handle].handle,
+    //     'count: ',
+    //     statsObjs[handle].count
+    //   );
+    //   num++;
+    // }
+
+    // Use forEach?
+
+    console.log('statsObj Testing  ', statsObjs['/10192/24386_2012_04']);
+
+    const statsPromises = handles.map(async handle => {
+      const objIdent = `'/${handle}${year_month}'`;
+      console.log('objIdent', objIdent);
+      let indObj = await statsObjs[objIdent];
+      //console.log(' indObj ', indObj, ' objIdent ', objIdent);
+      let count = 0;
+      if (indObj !== undefined) {
+        indObj.count = count;
+      } else {
+        count = indObj.count;
+      }
+
+      return handle + ',' + count + '/n';
+    });
+
+    const statsResolvedPromise = await Promise.all(statsPromises);
+    console.log(statsResolvedPromise);
+    fs.createWriteStream('./individualStats.csv', {
+      flags: 'a',
+    }).write(`${statsResolvedPromise}`);
   } catch (error) {
     console.error(error);
   }
