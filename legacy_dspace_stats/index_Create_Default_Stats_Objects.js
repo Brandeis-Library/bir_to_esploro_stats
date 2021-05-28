c39182946561017b6e7652c24039c30a322b1783
Chris Underwood <cunderwoodmn@gmail.com>
2021-05-27 17:19:58 -0700

    set up code to bring in the handles and write to the intended file.

--- legacy_dspace_stats/index_Create_Default_Stats_Objects.js
+++ legacy_dspace_stats/index_Create_Default_Stats_Objects.js
@@ -3,6 +3,16 @@ const fs = require('fs');
 (async function () {
   try {
     const { handles } = require('./handleList.js');
+
+    fs.truncateSync('./defaultDataObjects.js');
+
+    console.log('handles', handles);
+
+    await fs
+      .createWriteStream('./defaultDataObjects.js', {
+        flags: 'a',
+      })
+      .write(`${handles}`);
   } catch (error) {
     console.error(error);
   }
