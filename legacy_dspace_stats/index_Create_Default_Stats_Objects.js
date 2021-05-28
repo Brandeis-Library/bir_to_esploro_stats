e5053afadeea48fcc86ff39962254fec47572784
Chris Underwood <cunderwoodmn@gmail.com>
2021-05-27 18:41:42 -0700

    created default stats data objects.

--- legacy_dspace_stats/index_Create_Default_Stats_Objects.js
+++ legacy_dspace_stats/index_Create_Default_Stats_Objects.js
@@ -12,7 +12,18 @@ const fs = require('fs');
       })
       .write('module.exports = { defaultStatsObjs: {');
 
-    console.log('handles', handles);
+    //console.log('handles', handles);
+    let objString = '';
+    for (let i = 0; i < handles.length; i++) {
+      const obj = {};
+      const hand = handles[i];
+      obj.count = 0;
+      obj.handle = 'https://hdl.handle.net' + hand;
+      //console.log('default obj', obj);
+      fs.createWriteStream('./defaultDataObjects.js', {
+        flags: 'a',
+      }).write(`'${hand}': ${JSON.stringify(obj)},`);
+    }
 
     setTimeout(function () {
       fs.createWriteStream('./defaultDataObjects.js', {
